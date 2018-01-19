13ac83704838b002f35df0adbfb9398c1f332979
rashmi <rashmi@rashmi-ThinkPad-L570>
2018-01-19 14:57:39 +0530

    User Authentication Dto corrected

--- auth-service/src/main/java/com/desire3d/auth/dto/UsernameAuthentication.java
+++ auth-service/src/main/java/com/desire3d/auth/dto/UsernameAuthentication.java
@@ -15,10 +15,6 @@ public class UsernameAuthentication implements Serializable {
 
 	protected String loginId;
 
-	private String token;
-
-	private String newPassword;
-
 	public UsernameAuthentication() {
 		super();
 	}
@@ -28,35 +24,12 @@ public class UsernameAuthentication implements Serializable {
 		this.loginId = loginId;
 	}
 
-	public UsernameAuthentication(String loginId, String token, String newPassword) {
-		super();
-		this.loginId = loginId;
-		this.token = token;
-		this.newPassword = newPassword;
-	}
-
 	public String getLoginId() {
 		return loginId;
 	}
 
 	public void setLoginId(String loginId) {
 		this.loginId = loginId;
-	}
-
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
-	}
-
-	public String getNewPassword() {
-		return newPassword;
-	}
-
-	public void setNewPassword(String newPassword) {
-		this.newPassword = newPassword;
 	}
 
 }
