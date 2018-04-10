28e961b084485bc1d1823748534be33631d1f9f4
deepali-arvind <deepali.arvind@metamagic.in>
2018-04-10 19:36:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,7 @@ apt-get -y install zip unzip
 
 COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
 
-sed -i  "s|localhost:8081|config.desire3d.com:5030|g" auth-service/WEB-INF/classes/bootstrap.yml
-sed -i  "s|active: dev_env|active: qa_env|g" auth-service/WEB-INF/classes/bootstrap.yml
-sed -i "s|jdbc:postgresql:|jdbc:postgresql://db.desire3d.tech:8432/|g" auth-service/WEB-INF/classes/META-INF/persistence.xml
-sed -i "s|<property name="javax.jdo.option.ConnectionPassword" value="postgres" />|<property name="javax.jdo.option.ConnectionPassword" value="Meta#2k.18" />|g" auth-service/WEB-INF/classes/META-INF/persistence.xml
+
 
 cd auth-service/ && mvn clean install -Dmaven.test.skip=true
 
@@ -20,6 +17,10 @@ ls -la
 cp -rf target/auth-service-0.0.1-SNAPSHOT.war auth-service-docker/auth/
 
 cd auth-service-docker/auth/ && rm -rf auth-service && unzip -qq auth-service-0.0.1-SNAPSHOT.war -d auth-service && rm -rf auth-service-0.0.1-SNAPSHOT.war
+sed -i  "s|localhost:8081|config.desire3d.com:5030|g" auth-service/WEB-INF/classes/bootstrap.yml
+sed -i  "s|active: dev_env|active: qa_env|g" auth-service/WEB-INF/classes/bootstrap.yml
+sed -i "s|jdbc:postgresql:|jdbc:postgresql://db.desire3d.tech:8432/|g" auth-service/WEB-INF/classes/META-INF/persistence.xml
+sed -i "s|<property name="javax.jdo.option.ConnectionPassword" value="postgres" />|<property name="javax.jdo.option.ConnectionPassword" value="Meta#2k.18" />|g" auth-service/WEB-INF/classes/META-INF/persistence.xml
 
 cd ../ && git add auth && git commit -m "Automated commit on ${COMMIT_TIMESTAMP}" && git push
 cd\
