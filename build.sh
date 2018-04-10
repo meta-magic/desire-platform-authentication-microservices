8b88358248b77f0e4e89bfd1bd60b10ed455ccd4
deepali-arvind <deepali.arvind@metamagic.in>
2018-04-10 17:53:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,4 +18,4 @@ cp -rf target/auth-service-0.0.1-SNAPSHOT.war auth-service-docker/auth/
 
 cd auth-service-docker/auth/ && rm -rf auth-service && unzip -qq auth-service-0.0.1-SNAPSHOT.war -d auth-service && rm -rf auth-service-0.0.1-SNAPSHOT.war
 
-cd ../auth-service-docker/ && git add auth && git commit -m "Automated commit on ${COMMIT_TIMESTAMP}" && git push
+cd ../ && git add auth && git commit -m "Automated commit on ${COMMIT_TIMESTAMP}" && git push
